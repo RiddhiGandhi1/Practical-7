93bca301cc840f7206cd99f98831daf05cb03296
RiddhiGandhi1 <91381400+RiddhiGandhi1@users.noreply.github.com>
2021-12-11 19:41:16 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -11,7 +11,59 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+//import { useState } from "react";
+
+var CartFunctionality = () => {
+  var productNames = ["Jacuzzi", "Nike", "Adidas", "Lakme", "Bubble Wrap"];
+  var productPrices = [7500,8500,6500,9500,6700];
+  var productQty = [0,0,0,0,0];
+  var [qty, setQty] = React.useState(productQty);
+  var [totalQty, setTotalQty] = React.useState(0);  
+    
+ var calcluateQty = () => {
+   var total = 0;
+   qty.forEach((q) => {
+       total += q;
+    });
+   
+   return total;
+ };
+  
+ var calcluateAmount = () => {
+   var total = 0;
+   qty.forEach((q, index) => {
+       total += (q * productPrices[index]);
+    });
+   
+   return total;
+ };
+   
+  
+  var addToCart = (index) => {    
+    setQty(qty.map((q, i) => i == index ? q + 1 : q));       
+   };
+  
+  function removeCart(index) {
+      setQty(qty.map((q, i) => (i == index && q > 0) ? q - 1 : q));         
+  };
+  
+    return (      
+          <div>
+            {
+              productNames.map((nm, index) =>                               
+                              <div key={index}>
+                                <h3>{nm} : {productPrices[index]}
+                                  , Qty: { qty[index] }</h3>
+                                <button onClick={() => addToCart(index)}>Add</button>
+                                <button onClick={() => removeCart(index)}>Remove</button>
+                              </div>
+              )              
+            }
+          <h4>Total Qty: {calcluateQty()}</h4>
+          <h4>Total Amount: {calcluateAmount()}</h4>
+        </div>                               
+    );
+};
+
+ReactDOM.render(<CartFunctionality />, document.getElementById("root"));
 reportWebVitals();
